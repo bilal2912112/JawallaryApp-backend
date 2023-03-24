4eaeb808587f1a4d0500aca5cb9ed073a5906d10
bilal2912112 <bilaliqbal2912112@gmail.com>
2023-03-25 01:06:23 +0500

    Adding Auth feature

--- base/serializers.py
+++ base/serializers.py
@@ -3,6 +3,12 @@ from django.contrib.auth.models import User
 from .models import Product
 
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email']
+
+
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
--- base/views.py
+++ base/views.py
@@ -4,9 +4,24 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .products import products
 from .models import Product
-from .serializers import ProductSerializer
+from .serializers import ProductSerializer, UserSerializer
 
 # Create your views here.
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        data['username'] = self.user. username
+        data['email'] = self.user.email
+
+        return data
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
 
 
 @api_view(['GET'])
@@ -22,6 +37,14 @@ def getRoutes(request):
 
     ]
     return Response(routes)
+
+
+@api_view(['GET'])
+def getUserProfile(request):
+    user = request.user
+
+    serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
 
 
 @api_view(['GET'])
